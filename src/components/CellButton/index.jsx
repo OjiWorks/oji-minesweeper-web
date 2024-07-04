5307aa2114d7bb8caea56830e87d28d0e6612d29
Yangsubo <sheeep555@gmail.com>
2024-07-04 20:33:06 +0900

    Feat: 필드 시각화

---
+++ src/components/CellButton/index.jsx
@@ -0,0 +1,3 @@
+export default function CellButton({ value }) {
+  return <button className="w-6 h-6 bg-amber-700" >{value}</button>
+}
--- src/components/GameField/index.jsx
+++ src/components/GameField/index.jsx
@@ -2,9 +2,28 @@ import { useSelector } from "react-redux";
 
 export default function GameField() {
   const field = useSelector(state => state.bomb.field);
+  const { row, column } = useSelector(state => state.bomb.gameSetting);
+  console.log([row, column]);
+
   return (
-    <main>
-      메인보드
+    <main className="grid grid-cols-9 grid-rows-9 gap-1">
+      {field.coverField.map((columnArray, column) => {
+        return columnArray.map((element, row) => {
+          switch (element) {
+            case "covered":
+              return <button className="w-6 h-6 bg-amber-700"></button>;
+
+            case "flag":
+              return <button>flag</button>;
+
+            case "question":
+              return <button>question</button>;
+
+            case "open":
+              return <div>{field.underField[column][row]}</div>
+          }
+        });
+      })}
     </main>
   );
 }
