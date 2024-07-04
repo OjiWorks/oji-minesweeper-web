82d1d1a678eccaa94e4b81b254c932fd7b9a3e60
vlveqoo <vlveqoo@gmail.com>
2024-07-04 18:59:05 +0900

    Design: 파비콘지정 및 css중앙정렬등

--- src/components/Entrance/index.jsx
+++ src/components/Entrance/index.jsx
@@ -1,6 +1,7 @@
 import { useDispatch } from "react-redux";
 import { setGameInfo, toggleView } from "../../services/bomb";
 
+import logo from "../../assets/textlogo.png"
 import convertToBombRate from "../utils/converToBombRate";
 
 export default function Entrance() {
@@ -21,27 +22,31 @@ export default function Entrance() {
   }
 
   return (
-    <div className="md:flex flex flex-col justify-center items-center bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-400">
-      <h1>BombYangGang</h1>
-      <form onSubmit={handleGameStart}>
-        <div>
-          유저이름 : <br/>
-          <input type="text" defaultValue="의성짱짱맨" required />
-        </div>
-        <div>
-          가로 :
-          <input min="9" max="30" defaultValue="9" type="number" required/>
-          세로 :
-          <input min="9" max="30" defaultValue="9" type="number" required/>
-          난이도 :
-          <select required>
-            <option>초급</option>
-            <option>중급</option>
-            <option>고급</option>
-          </select>
-        </div>
-        <button className="h-10 px-6 font-semibold rounded-md bg-black text-white" type="submit">게임시작</button>
-      </form>
+    <div className="fixed inset-0 flex flex-col items-center justify-center ">
+      <div className="flex flex-col justify-center items-center p-6 bg-slate-100 rounded-xl dark:bg-slate-400">
+        <img src={logo} />
+        <form onSubmit={handleGameStart} className="flex flex-col items-center">
+          <div className="my-3">
+            유저이름 <input type="text" defaultValue="의성짱짱맨" className="text-center" required />
+          </div>
+          <div className="my-3">
+            가로
+            <input min="9" max="30" defaultValue="9" type="number" className="text-center" required />칸
+            세로
+            <input min="9" max="30" defaultValue="9" type="number" className="text-center" required />칸
+            <div className="my-3">
+              난이도
+              <select required>
+                <option>초급</option>
+                <option>중급</option>
+                <option>고급</option>
+              </select>
+            </div>
+          </div>
+          <button className="h-10 px-6 font-semibold rounded-md bg-black text-white" type="submit">게임시작</button>
+        </form>
+      </div>
     </div>
+
   );
 }
