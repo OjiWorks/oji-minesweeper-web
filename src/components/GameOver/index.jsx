18bf04800b332aa50c99a87fc72377951b19d1c5
vlveqoo <vlveqoo@gmail.com>
2024-07-05 19:18:31 +0900

    Design: 게임필드 화면에 다시하기, 메인으로 버튼 생성

--- src/components/GameOver/index.jsx
+++ src/components/GameOver/index.jsx
@@ -4,7 +4,7 @@ import gameOverLottie from "../../assets/game_over.json";
 export default function GameOver() {
   return (
     <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-300 h-200" >
-      <button className="absolute top-1/2 transform -translate-x-1/2 -translate-y-1/2 left-1/2 z-10 h-10 px-6 font-semibold rounded-md bg-black text-white" onClick={() => location.reload(true)} >다시하기</button>
+      <button className="custom-blackButton absolute top-1/2 transform -translate-x-1/2 -translate-y-1/2 left-1/2" onClick={() => location.reload(true)} >다시하기</button>
       <Lottie
         animationData={gameOverLottie} loop={false} />
     </div>
