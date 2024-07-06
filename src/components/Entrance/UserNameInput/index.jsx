68dff2153351e0cc11191fa3783fa97daed5d6d2
Yangsubo <sheeep555@gmail.com>
2024-07-06 10:23:47 +0900

    Feat: 게임 승리시 유저아이디 표시 및 클리어시간 표시

--- src/components/Entrance/UserNameInput/index.jsx
+++ src/components/Entrance/UserNameInput/index.jsx
@@ -4,7 +4,7 @@ export default function UseNameInput() {
       <label>
         유저이름
       </label>
-      <input type="text" defaultValue="의성짱짱맨" className="text-center" required />
+      <input type="text" placeholder="이름을 입력해주세요" className="text-center" required maxLength={5}/>
     </div>
   );
 }
