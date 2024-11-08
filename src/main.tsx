52a0d13ec51ef8debe1ca8ac633a4a19dd900a93
coco8j <dlwndusco38@gmail.com>
2024-11-08 14:02:24 +0900

    Refactor: main 파일 ts 변환

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import store from "./store";
 
 import "./input.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
