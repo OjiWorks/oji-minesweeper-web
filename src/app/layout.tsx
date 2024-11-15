48e59246c99ad23abbbaaf2ce4062b53018935dc
coco8j <dlwndusco38@gmail.com>
2024-11-15 14:24:20 +0900

    Fix: 파비콘 미출력 현상 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,14 +5,11 @@ import { Provider } from "react-redux";
 import store from "@src/store";
 import "./globals.css";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="kr">
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <title>BombYangGang</title>
       </head>
       <body>
