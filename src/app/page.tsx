55066da0634eed5f7e964ddfe26646c292cf1e11
YS <allansad12@gmail.com>
2024-11-18 18:03:31 +0900

    Refactor: 메인화면 라우트 조건 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,36 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { createClient } from "@/src/utils/supabase/client";
 
 export default function Home() {
+  const supabase = createClient();
   const router = useRouter();
 
   useEffect(() => {
-    const accessToken = localStorage.getItem("accessToken");
+    const checkAuth = async () => {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
 
-    if (!accessToken) {
-      router.push("/user/login");
-    } else {
-      router.push("/game");
-    }
+      if (user) {
+        router.push("/game");
+      } else {
+        router.push("/user/login");
+      }
+    };
+
+    checkAuth();
   }, []);
-  return <div>BombYangGang</div>;
+
+  return (
+    <div className="fixed inset-0 flex flex-col items-center justify-center">
+      <div className="flex flex-col justify-center items-center p-6 bg-orange-400 rounded-xl">
+        <div className="flex flex-col items-center justify-center p-6">
+          <p>...Loading</p>
+        </div>
+      </div>
+    </div>
+  );
 }
