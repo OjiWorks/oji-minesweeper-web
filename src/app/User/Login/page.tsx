21b4ec078fa60ab84a7b95de21ad5b718158b78f
YS <allansad12@gmail.com>
2024-11-18 19:13:21 +0900

    Refactor: 회원가입 및 로그인 name 속성 추가 및 server action 연결

--- src/app/User/Login/page.tsx
+++ src/app/User/Login/page.tsx
@@ -3,15 +3,24 @@
 import Button from "@components/Button";
 import { TextInput } from "@components/TextInput";
 
+import login from "./actions";
+
 export default function Login() {
   return (
     <div className="fixed inset-0 flex flex-col items-center justify-center">
       <div className="flex flex-col justify-center items-center p-6 bg-orange-400 rounded-xl">
         <form className="flex flex-col items-center p-10">
-          <TextInput label="이메일" placeholder="이메일을 입력하세요" />
-          <TextInput label="비밀번호" placeholder="비밀번호를 입력하세요" />
-          {/* TODO: onClick시 auth api 요청 연결하기 */}
-          <Button text="로그인하기" />
+          <TextInput
+            label="이메일"
+            name="email"
+            placeholder="이메일을 입력하세요"
+          />
+          <TextInput
+            label="비밀번호"
+            name="password"
+            placeholder="비밀번호를 입력하세요"
+          />
+          <Button formAction={login} text="로그인하기" />
         </form>
       </div>
     </div>
--- src/app/User/Register/page.tsx
+++ src/app/User/Register/page.tsx
@@ -3,19 +3,36 @@
 import Button from "@components/Button";
 import { TextInput } from "@components/TextInput";
 
+import register from "./actions";
+
 export default function Register() {
   return (
     <div className="fixed inset-0 flex flex-col items-center justify-center">
       <div className="flex flex-col justify-center items-center p-6 bg-orange-400 rounded-xl">
         <form className="flex flex-col items-center p-10">
           <div className="my-3">
-            <TextInput label="이메일" placeholder="사용할 이메일을 입력해주세요" />
-            <TextInput label="닉네임" placeholder="사용할 닉네임을 입력해주세요" />
-            <TextInput label="비밀번호" placeholder="비밀번호를 입력해주세요" />
-            <TextInput label="비밀번호 확인" placeholder="비밀번호를 재입력해주세요" />
+            <TextInput
+              label="이메일"
+              name="email"
+              placeholder="사용할 이메일을 입력해주세요"
+            />
+            <TextInput
+              label="닉네임"
+              name="username"
+              placeholder="사용할 닉네임을 입력해주세요"
+            />
+            <TextInput
+              label="비밀번호"
+              name="password"
+              placeholder="비밀번호를 입력해주세요"
+            />
+            <TextInput
+              label="비밀번호 확인"
+              name="passwordConfirm"
+              placeholder="비밀번호를 재입력해주세요"
+            />
           </div>
-          {/* TODO: onClick 요청 연결하기 */}
-          <Button text="등록하기" />
+          <Button formAction={register} text="등록하기" />
         </form>
       </div>
     </div>
