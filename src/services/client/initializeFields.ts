5dd8ebea88d50814d8fb157da5a9e8ab1d6c7b18
YS <allansad12@gmail.com>
2024-11-23 21:42:57 +0900

    Refactor: initializeFields에 seed 추가

--- src/services/client/initializeFields.ts
+++ src/services/client/initializeFields.ts
@@ -1,9 +1,16 @@
 import { CoverState } from "@/src/types";
 import createUnderField from "./createField";
 
-export default function initializeFields(row: number, column: number, difficulty: number) {
-  const underField = createUnderField(row, column, difficulty);
-  const coverField = Array(column).fill(Array(row).fill("covered")) as CoverState[][];
+export default function initializeFields(
+  row: number,
+  column: number,
+  difficulty: number,
+  seed?: string
+) {
+  const underField = createUnderField(row, column, difficulty, seed);
+  const coverField = Array(column).fill(
+    Array(row).fill("covered")
+  ) as CoverState[][];
 
   return { underField, coverField };
 }
