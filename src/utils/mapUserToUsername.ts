b96913737adda7d4f7f28f0eb9bf7df867a9ac54
YS <allansad12@gmail.com>
2024-12-14 10:34:31 +0900

    Refactor: mapUserToUsername 함수가 명시적인 타입을 반환하도록 수정

--- src/utils/mapUserToUsername.ts
+++ src/utils/mapUserToUsername.ts
@@ -1,14 +1,45 @@
-type PropsTypes = {
-  daily_score?: number;
-  total_score?: number;
+type Daily = {
+  daily_score: number;
+  user: { username: string };
+};
+type Total = {
+  total_score: number;
   user: { username: string };
 };
 
-export default function mapUserToUsername(scoreData: PropsTypes[]) {
+export type MappedDaily = {
+  daily_score: number;
+  username: string;
+};
+
+export type MappedTotal = {
+  total_score: number;
+  username: string;
+};
+
+export default function mapUserToUsername(scoreData: Daily[]): MappedDaily[];
+export default function mapUserToUsername(scoreData: Total[]): MappedTotal[];
+export default function mapUserToUsername(
+  scoreData: Daily[] | Total[]
+): MappedDaily[] | MappedTotal[] {
+  function isDailyArray(items: (Daily | Total)[]): items is Daily[] {
+    return items.every((data) => "daily_score" in data);
+  }
+
+  if (isDailyArray(scoreData)) {
+    return scoreData.map((data) => {
+      const { user, daily_score } = data;
+      return {
+        daily_score,
+        username: user.username,
+      };
+    });
+  }
+
   return scoreData.map((data) => {
-    const { user, ...rest } = data;
+    const { user, total_score } = data;
     return {
-      ...rest,
+      total_score,
       username: user.username,
     };
   });
